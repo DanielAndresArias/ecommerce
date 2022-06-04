6722955b31e305aff54e68dc6eedbc879cdd891d
Skaelet <danielandres.98.da@gmail.com>
2022-06-04 20:32:44 -0300

    Creación de componente Header. Se implementaron estilos temporales con sass. Falta elegir bien la paleta de colores.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './css/style.css';
-import NavBar from './components/NavBar/NavBar';
+import Header from './components/Header/Header';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <main>
-      <header className='header'>
-        <h2 className='name'>MantasXL.Deco</h2>
-        <NavBar/>
-      </header>
-
+      <Header/>
       <ItemListContainer/>
     </main>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import NavBar from '../NavBar/NavBar';
+
+const Header = () =>{
+
+    return(
+        <header className='header'>
+            <div>
+                <img src='logo.PNG'/>
+            </div>
+            <NavBar/>
+        </header>
+    )
+}
+
+export default Header;
